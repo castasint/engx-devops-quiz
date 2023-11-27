68907512536bb378a4ca6eaaf0531d2fbc85a9ca
Hanuma <castasstring@gmail.com>
2023-11-27 10:01:38 +0530

    Update data.js

--- src/app/data.js
+++ src/app/data.js
@@ -7,18 +7,19 @@ const devOpsQuestions = [
       "DevOps is primarily focused on faster code deployment, regardless of the collaboration between development and operations teams.",
       "DevOps is a philosophy that emphasizes collaboration between software development and IT operations teams, integrating practices like automation and continuous delivery to improve the speed and quality of software development and deployment."
     ],
-    correctAnswerIndex: 3
+    correctAnswerIndex: 3,
+
   },
-  {
-    question: "What is the key advantage of a Blue/Green deployment strategy in DevOps?",
-    options: [
-      "It allows simultaneous deployment of two different versions of an application.",
-      "It eliminates downtime and reduces the risk of introducing a new version.",
-      "It's specifically designed for hotfixes and quick patches.",
-      "It enhances collaboration between development and operations teams."
-    ],
-    correctAnswerIndex: 1
-  },
+  /*   {
+      question: "What is the key advantage of a Blue/Green deployment strategy in DevOps?",
+      options: [
+        "It allows simultaneous deployment of two different versions of an application.",
+        "It eliminates downtime and reduces the risk of introducing a new version.",
+        "It's specifically designed for hotfixes and quick patches.",
+        "It enhances collaboration between development and operations teams."
+      ],
+      correctAnswerIndex: 1
+    }, */
   {
     question: "In the context of DevOps, what does 'Shift Left' mean?",
     options: [
@@ -39,26 +40,26 @@ const devOpsQuestions = [
     ],
     correctAnswerIndex: 2
   },
-  {
-    question: "In DevOps, what is Canary Testing??",
-    options: [
-      "A testing approach where the entire application is tested at once.",
-      "Releasing a new feature to a small group of users to assess its impact before wider deployment.",
-      "A type of security testing to check for vulnerabilities.",
-      "Automated testing that is done before code is merged into the main branch."
-    ],
-    correctAnswerIndex: 1
-  },
-  {
-    question: "What does the term 'Immutable Infrastructure' refer to in a DevOps context?",
-    options: [
-      "Infrastructure that cannot be modified once it is deployed.",
-      "The practice of regularly updating and changing the infrastructure.",
-      "Infrastructure components that are resistant to change.",
-      "A security practice to make infrastructure tamper-proof."
-    ],
-    correctAnswerIndex: 0
-  }
+  /*   {
+      question: "In DevOps, what is Canary Testing??",
+      options: [
+        "A testing approach where the entire application is tested at once.",
+        "Releasing a new feature to a small group of users to assess its impact before wider deployment.",
+        "A type of security testing to check for vulnerabilities.",
+        "Automated testing that is done before code is merged into the main branch."
+      ],
+      correctAnswerIndex: 1
+    }, */
+  /*   {
+      question: "What does the term 'Immutable Infrastructure' refer to in a DevOps context?",
+      options: [
+        "Infrastructure that cannot be modified once it is deployed.",
+        "The practice of regularly updating and changing the infrastructure.",
+        "Infrastructure components that are resistant to change.",
+        "A security practice to make infrastructure tamper-proof."
+      ],
+      correctAnswerIndex: 0
+    } */
 ];
 
 export default devOpsQuestions;
